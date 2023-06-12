97c4e0f3db13d6c3692e8b5bb2693c4187f47951
Yukiman42 <benjh.9742@gmail.com>
2023-06-12 11:14:39 +0200

    commit 3

--- ProjetAfficheurLED/Form1.Designer.cs
+++ ProjetAfficheurLED/Form1.Designer.cs
@@ -29,16 +29,10 @@
         private void InitializeComponent()
         {
             this.sendBtn = new System.Windows.Forms.Button();
-            this.textPort = new System.Windows.Forms.TextBox();
             this.panelLed = new System.Windows.Forms.Panel();
-            this.listFiles = new System.Windows.Forms.ListBox();
-            this.saveBtn = new System.Windows.Forms.Button();
-            this.loadBtn = new System.Windows.Forms.Button();
-            this.textFileName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.clearBtn = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.deleteBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textPort = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // sendBtn
@@ -52,15 +46,6 @@
             this.sendBtn.UseVisualStyleBackColor = true;
             this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
             // 
-            // textPort
-            // 
-            this.textPort.Location = new System.Drawing.Point(128, 279);
-            this.textPort.Margin = new System.Windows.Forms.Padding(4);
-            this.textPort.Name = "textPort";
-            this.textPort.Size = new System.Drawing.Size(132, 22);
-            this.textPort.TabIndex = 1;
-            this.textPort.Text = "4444";
-            // 
             // panelLed
             // 
             this.panelLed.Location = new System.Drawing.Point(16, 15);
@@ -68,57 +53,6 @@
             this.panelLed.Name = "panelLed";
             this.panelLed.Size = new System.Drawing.Size(1015, 255);
             this.panelLed.TabIndex = 3;
-            // 
-            // listFiles
-            // 
-            this.listFiles.FormattingEnabled = true;
-            this.listFiles.ItemHeight = 16;
-            this.listFiles.Location = new System.Drawing.Point(1039, 15);
-            this.listFiles.Margin = new System.Windows.Forms.Padding(4);
-            this.listFiles.Name = "listFiles";
-            this.listFiles.Size = new System.Drawing.Size(279, 260);
-            this.listFiles.TabIndex = 6;
-            // 
-            // saveBtn
-            // 
-            this.saveBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.saveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saveBtn.Location = new System.Drawing.Point(1211, 309);
-            this.saveBtn.Margin = new System.Windows.Forms.Padding(4);
-            this.saveBtn.Name = "saveBtn";
-            this.saveBtn.Size = new System.Drawing.Size(107, 38);
-            this.saveBtn.TabIndex = 7;
-            this.saveBtn.Text = "Sauvegarder";
-            this.saveBtn.UseVisualStyleBackColor = true;
-            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
-            // 
-            // loadBtn
-            // 
-            this.loadBtn.Location = new System.Drawing.Point(1060, 310);
-            this.loadBtn.Margin = new System.Windows.Forms.Padding(4);
-            this.loadBtn.Name = "loadBtn";
-            this.loadBtn.Size = new System.Drawing.Size(143, 37);
-            this.loadBtn.TabIndex = 8;
-            this.loadBtn.Text = "Charger un fichier";
-            this.loadBtn.UseVisualStyleBackColor = true;
-            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
-            // 
-            // textFileName
-            // 
-            this.textFileName.Location = new System.Drawing.Point(1167, 279);
-            this.textFileName.Margin = new System.Windows.Forms.Padding(4);
-            this.textFileName.Name = "textFileName";
-            this.textFileName.Size = new System.Drawing.Size(151, 22);
-            this.textFileName.TabIndex = 10;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 281);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 17);
-            this.label1.TabIndex = 12;
-            this.label1.Text = " Port d\'écoute : ";
             // 
             // clearBtn
             // 
@@ -130,39 +64,31 @@
             this.clearBtn.UseVisualStyleBackColor = true;
             this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
             // 
-            // label2
+            // label1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(1019, 281);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 20);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "Nom du fichier :";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = " Port d\'écoute : ";
             // 
-            // deleteBtn
+            // textPort
             // 
-            this.deleteBtn.Location = new System.Drawing.Point(904, 310);
-            this.deleteBtn.Name = "deleteBtn";
-            this.deleteBtn.Size = new System.Drawing.Size(149, 37);
-            this.deleteBtn.TabIndex = 15;
-            this.deleteBtn.Text = "Supprimer un fichier";
-            this.deleteBtn.UseVisualStyleBackColor = true;
-            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            this.textPort.Location = new System.Drawing.Point(128, 279);
+            this.textPort.Margin = new System.Windows.Forms.Padding(4);
+            this.textPort.Name = "textPort";
+            this.textPort.Size = new System.Drawing.Size(132, 22);
+            this.textPort.TabIndex = 1;
+            this.textPort.Text = "4444";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1335, 356);
-            this.Controls.Add(this.deleteBtn);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(1046, 356);
             this.Controls.Add(this.clearBtn);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textFileName);
-            this.Controls.Add(this.loadBtn);
-            this.Controls.Add(this.saveBtn);
-            this.Controls.Add(this.listFiles);
             this.Controls.Add(this.panelLed);
             this.Controls.Add(this.textPort);
             this.Controls.Add(this.sendBtn);
@@ -177,16 +103,10 @@
         #endregion
 
         private System.Windows.Forms.Button sendBtn;
-        private System.Windows.Forms.TextBox textPort;
         private System.Windows.Forms.Panel panelLed;
-        private System.Windows.Forms.ListBox listFiles;
-        private System.Windows.Forms.Button saveBtn;
-        private System.Windows.Forms.Button loadBtn;
-        private System.Windows.Forms.TextBox textFileName;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button clearBtn;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textPort;
     }
 }
 
--- ProjetAfficheurLED/Form1.cs
+++ ProjetAfficheurLED/Form1.cs
@@ -17,42 +17,30 @@ namespace ProjetAfficheurLED
         TcpSocket tcpSocket = new TcpSocket();
 
         int size = 18;
-        int columns = 44;
-        int rows = 11;
+        int col = 44;
+        int line = 11;
 
         PictureBox[] tabLed;
         Color backgroundColor = Color.White;
         Color selectedColor = Color.Blue;
         int[] ledValue;
 
-        string folderName = @"C:\MessagesLED";
-
         public Form1()
         {
             InitializeComponent();
-            tabLed = new PictureBox[rows * columns];
-            ledValue = new int[rows * columns];
+            tabLed = new PictureBox[line * col];
+            ledValue = new int[line * col];
             for (int iIndex = 0; iIndex < tabLed.Length; iIndex++)
             {
                 ledValue[iIndex] = 0;
                 tabLed[iIndex] = new PictureBox();
                 tabLed[iIndex].Name = iIndex.ToString();
-                tabLed[iIndex].Location = new Point(6 + (iIndex * size % (columns * size)), 6 + size * (iIndex * size / (columns * size)));
+                tabLed[iIndex].Location = new Point(6 + (iIndex * size % (col * size)), 6 + size * (iIndex * size / (col * size)));
                 tabLed[iIndex].Size = new Size(size, size);
                 tabLed[iIndex].BackColor = backgroundColor;
                 tabLed[iIndex].BorderStyle = BorderStyle.Fixed3D;
                 tabLed[iIndex].MouseClick += new MouseEventHandler(pictureBox_MouseClick);
                 panelLed.Controls.Add(tabLed[iIndex]);
-            }
-
-            if (!Directory.Exists(folderName))
-            {
-                Directory.CreateDirectory(folderName);
-            }
-
-            foreach (string file in Directory.GetFiles(folderName))
-            {
-                listFiles.Items.Add(Path.GetFileName(file));
             }
         }
 
@@ -82,75 +70,6 @@ namespace ProjetAfficheurLED
                 picutreBox.BackColor = backgroundColor;
                 ledValue[Array.IndexOf(tabLed, picutreBox)] = 0;
             }
-        }
-
-        private void saveBtn_Click(object sender, EventArgs e)
-        {
-            string fileName = folderName + @"\" + textFileName.Text + ".data";
-            string data = "";
-            foreach (int value in ledValue)
-            {
-                data += value.ToString();
-            }
-
-            if ( textFileName.Text == "")
-            { 
-                MessageBox.Show("Nom de fichier vide");
-            }
-            else if (!File.Exists(fileName) && textFileName.Text != "")
-            {
-                File.WriteAllText(fileName, data);
-                listFiles.Items.Add(Path.GetFileName(fileName));
-                MessageBox.Show("Fichier sauvegardé");
-            }
-            else
-            {
-                MessageBox.Show("Ce fichier existe déjà");
-            }
-        }
-
-        private void loadBtn_Click(object sender, EventArgs e)
-        {
-            string fileName = folderName + @"\" + listFiles.GetItemText(listFiles.SelectedItem);
-            if (File.Exists(fileName))
-            {
-                string data = File.ReadAllText(fileName);
-                for (int i = 0; i < data.Length; i++)
-                {
-                    int value = int.Parse(data[i].ToString());
-                    PictureBox picutreBox = tabLed[i];
-
-                    if (value == 1)
-                    {
-                        picutreBox.BackColor = selectedColor;
-                        ledValue[i] = 1;
-                    }
-                    else
-                    {
-                        picutreBox.BackColor = backgroundColor;
-                        ledValue[i] = 0;
-                    }
-                }
-            }
-        }
-
-        private void deleteBtn_Click(object sender, EventArgs e)
-        {
-            string fileName = folderName + @"\" + listFiles.GetItemText(listFiles.SelectedItem);
-            if (File.Exists(fileName))
-            {
-                try
-                {
-                    File.Delete(fileName);
-                    listFiles.Items.Remove(listFiles.SelectedItem);
-                }
-                catch (IOException ex)
-                {
-                    // Gérer les exceptions liées à la suppression du fichier
-                    MessageBox.Show("Une erreur s'est produite lors de la suppression du fichier : " + ex.Message);
-                }
-            }
-
         }
 
         private void clearBtn_Click(object sender, EventArgs e)
